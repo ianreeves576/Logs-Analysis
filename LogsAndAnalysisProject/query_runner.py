a278070821640d22a257a8843f570cfee000cdfd
Ian Reeves <ianreeves576@hotmail.com>
2017-08-24 12:49:58 -0700

    Delete improper query_runner.py

--- LogsAndAnalysisProject/query_runner.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-
-from modules import top_articles, top_authors, errors_per_day
-
-top_articles.print_top_articles()
-top_authors.print_top_authors()
-errors_per_day.print_errors_per_day()
