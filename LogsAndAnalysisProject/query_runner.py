b9f52162dff3f615d85045e1802f2a3dde4e72a1
Ian Reeves <ianreeves576@hotmail.com>
2017-08-24 12:53:41 -0700

    Uploaded new query_runner.py

---
+++ LogsAndAnalysisProject/query_runner.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3.6
+
+from modules import top_articles, top_authors, errors_per_day
+
+top_articles.print_top_articles()
+top_authors.print_top_authors()
+errors_per_day.print_errors_per_day()
